dae805fc197cce403f32523f09a593e41567900f
Zoro <jagbetuyi001@gmail.com>
2024-08-20 09:42:56 +0100

    test CI/CD with vercel deployment

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -4,7 +4,7 @@ import banner from "../../public/images/banner-image.png";
 
 const HomeBanner = () => {
 	return (
-		<div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
+		<div className="relative bg-gradient-to-r from-lime-700 to-lime-500 mb-8">
 			<div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
 				<div className="mb-8 md:mb-0 text-center">
 					<h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
